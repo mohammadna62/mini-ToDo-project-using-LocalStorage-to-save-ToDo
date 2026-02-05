fb76b2f4ccec5821a63d9b4223ef8a3785efa14a
mohammad naghavi <mohammad.naghavi@gmail.com>
2026-02-05 10:53:58 +0330

    add date to task(todo list) and add sort option (completed , uncompleted and all task)

--- js/app.js
+++ js/app.js
@@ -1,107 +1,181 @@
-const modalScreen = document.querySelector(".modal-screen");
+const modal = document.querySelector(".modal-screen");
 const openModalBtn = document.querySelector(".open-modal-button");
-const closeXModal = document.querySelector(".close-x-modal");
 const cancelTodoBtn = document.querySelector(".cancel");
+const closeXModal = document.querySelector(".close-x-modal");
 const createTodoBtn = document.querySelector(".create");
-const todoInput = document.querySelector("input");
+const todoInput = document.querySelector(".input");
 const todosContainer = document.querySelector(".todos-container");
+const sortBtns = document.querySelectorAll(".sort-menu button");
+const sortTypeElem = document.querySelector(".sort-type");
+
+//*<------------- Initialization Zone ---------------->
+
+const now = new Date();
 let todos = [];
+const jalaliDate = new Intl.DateTimeFormat("fa-IR", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+}).format(now);
+
+const time = new Intl.DateTimeFormat("fa-IR", {
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: false, // 24 ساعته
+}).format(now);
+
 //!<----------Function Zone--------->
+
 function showModal() {
-  modalScreen.classList.remove("hidden");
+  modal.classList.remove("hidden");
 }
+
 function hideModal() {
-  modalScreen.classList.add("hidden");
+  modal.classList.add("hidden");
 }
+
 function addTodo() {
   const todoTitle = todoInput.value;
+
   const newTodo = {
     id: Math.floor(Math.random() * 9999),
     title: todoTitle,
     isComplete: false,
+    date: `${jalaliDate} , ${time}`
   };
+
   todos.push(newTodo);
-  console.log(todos);
   todoInput.value = "";
+
   saveInToLocalStorage(todos);
-  showTodos();
+  showTodos(todos);
   hideModal();
 }
 
-function showTodos() {
+function showTodos(shownTodos) {
   todosContainer.innerHTML = "";
-  if(todos.length){
-     todos.forEach(function (todo) {
-    todosContainer.insertAdjacentHTML(
-      "beforeend",
-      `
-      <article  class="todo ${todo.isComplete ? 'complete' : ''}" >
-          <div class="todo-data">
-            <div class="checkbox">
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="size-4"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m4.5 12.75 6 6 9-13.5"
-                  />
-                </svg>
-              </span>
-            </div>
-            <div>
-              <p class="todo-title">${todo.title}</p>
-            </div>
-          </div>
 
-          <div class="todo-buttons">
-            <button class="delete" onclick="removeTodo(${todo.id})" >حذف</button>
-            <div class="relative">
-              <button class="status">وضعیت</button>
-              <ui class="status-menu">
-                <li value="default">معمولی</li>
-                <li value="important">مهم</li>
-                <li value="very-important">خیلی مهم</li>
-              </ui>
+  if (shownTodos.length) {
+    shownTodos.forEach(function (todo) {
+      todosContainer.insertAdjacentHTML(
+        "beforeend",
+        `
+          <article class="todo ${todo.isComplete ? "complete" : ""}">
+            <div class="todo-data">
+              <div class="checkbox">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m4.5 12.75 6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+              </div>
+              <div>
+                <p class="todo-title">${todo.title}</p>
+                <div class="date">${todo.date}</div>
+              </div>
             </div>
-            <button class="complete">تکمیل</button>
-          </div>
-        </article>
-  `,
-    );
-  });
-  }else{
-    todosContainer.innerHTML = `<h1 style = "text-align : center;color : red">محتوایی یافت نشد</h1>`
+  
+            <div class="todo-buttons">
+              <button class="delete" onclick="removeTodo(${
+                todo.id
+              })">حذف</button>
+              <button class="complete" onclick="completeTodo(${
+                todo.id
+              })">تکمیل</button>
+            </div>
+          </article>
+        `
+      );
+    });
+  } else {
+    todosContainer.innerHTML = `<h1 style="text-align:center;color:red">محتوایی یافت نشد</h1>`;
   }
- 
 }
-function removeTodo(todoId){
- const mainTodoIndex =  todos.findIndex(function(todo){
-    return todo.id=== todoId
-   })
-   todos.splice(mainTodoIndex,1)
-   showTodos()
-   saveInToLocalStorage(todos)
+
+function removeTodo(todoId) {
+  const mainTodoIndex = todos.findIndex(function (todo) {
+    return todo.id === todoId;
+  });
+
+  todos.splice(mainTodoIndex, 1);
+  showTodos(todos);
+  saveInToLocalStorage(todos);
 }
+
+function completeTodo(todoId) {
+  todos.some(function (todo) {
+    if (todo.id === todoId) {
+      todo.isComplete = true;
+      return true;
+    }
+  });
+
+  saveInToLocalStorage(todos);
+  showTodos(todos);
+}
+
 function saveInToLocalStorage(todosArray) {
   localStorage.setItem("todos", JSON.stringify(todosArray));
 }
+
 function getDataFromLocalStorage() {
   const localTodos = JSON.parse(localStorage.getItem("todos"));
-  if(localTodos){
-    todos = localTodos
+
+  if (localTodos) {
+    todos = localTodos;
   }
-  showTodos()
+
+  showTodos(todos);
+}
+
+function todoSortHandler(event) {
+  const sortType = event.target.value;
+  const sortTitle = event.target.innerHTML;
+
+  switch (sortType) {
+    case "completed": {
+      const completedTodos = todos.filter(function (todo) {
+        return todo.isComplete === true;
+      });
+
+      sortTypeElem.innerHTML = sortTitle;
+      showTodos(completedTodos);
+      break;
+    }
+
+    case "uncompleted": {
+      const unCompletedTodos = todos.filter(function (todo) {
+        return todo.isComplete === false;
+      });
+
+      sortTypeElem.innerHTML = sortTitle;
+      showTodos(unCompletedTodos);
+      break;
+    }
+
+    default: {
+      sortTypeElem.innerHTML = sortTitle;
+      showTodos(todos);
+    }
+  }
 }
 //?<----------------Event Zone--------------->
+sortBtns.forEach(function (sortBtn) {
+  sortBtn.addEventListener("click", todoSortHandler);
+});
 
 openModalBtn.addEventListener("click", showModal);
-closeXModal.addEventListener("click", hideModal);
 cancelTodoBtn.addEventListener("click", hideModal);
 createTodoBtn.addEventListener("click", addTodo);
+closeXModal.addEventListener("click", hideModal);
